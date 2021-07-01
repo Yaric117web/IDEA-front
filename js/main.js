d8db91f74be51fc77e43ff11751fa5353471ae0e
EVGENIY PASHKOV <61497581+pashkoveval@users.noreply.github.com>
2021-07-01 11:09:51 +0300

    Heder and First Block-slider

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// Menu open on click
+$(".header__button").on("click", function () {
+    $(".header-dropmenu").addClass("open")
+    $(".header-closemenu").addClass("open")
+    $(".header-mask").addClass("open")
+    $("body").addClass("open")
+
+})
+$(".header-closemenu").on("click", function () {
+    $(".header-dropmenu").removeClass("open")
+    $(".header-closemenu").removeClass("open")
+    $(".header-mask").removeClass("open")
+    $("body").removeClass("open")
+})
+$(".header-mask").on("click", function () {
+    $(".header-dropmenu").removeClass("open")
+    $(".header-closemenu").removeClass("open")
+    $(".header-mask").removeClass("open")
+    $("body").removeClass("open")
+})
