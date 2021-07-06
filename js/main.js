6db22d1150e73469d5fe88ac491fc678ca150a08
EVGENIY PASHKOV <61497581+pashkoveval@users.noreply.github.com>
2021-07-07 02:45:59 +0300

    Start adapt footer. Сonnected slick and your fancybox, looks mach better but masonry still have a problem.

--- js/main.js
+++ js/main.js
@@ -1,18 +1,45 @@
 'use strict'
-/**
- * Menu open on click
- */
-$(".header__button").on("click", function () {
-    $("body").addClass("open")
-})
 
+$(".regular").slick({
+    dots: false,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    arrows: true,
+    lazyLoad: true,
+    mobileFirst: true,
+    infinite: true,
+    responsive: [
+        {
+            breakpoint: 1020,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 280,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+    ]
+});
 
-/**
- * Masonry lib option
- */
 $('.grid').masonry({
-    columnWidth: '.grid-sizer',
-    gutter: '.gutter-sizer',
     itemSelector: '.grid-item',
-    percentPosition: true
+    columnWidth: '.grid-item',
+    percentPosition: true,
+    gutter: 15,
 });
